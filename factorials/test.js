729d9eac958b3714969722160a3e9ebea568a9f5
Paul Milbank <paulmilbank@gmail.com>
2017-10-25 22:08:41 +1300

    0 is valid for factorials

--- factorials/test.js
+++ factorials/test.js
@@ -8,12 +8,12 @@ describe('factorial', () => {
   })
 
   it('Should throw if given an out of bounds input.', () => {
-    expect(() => factorial(0)).to.throw('Factorial needs to be a positive number greater than 1.')
-    expect(() => factorial(0.5)).to.throw('Factorial needs to be a positive number greater than 1.')
     expect(() => factorial('meow')).to.throw('Factorial needs to be a positive number greater than 1.')
   })
 
   it('should return a correct factorial', () => {
+    expect(factorial(0)).to.equal(1) // weird maths edge case.
+    expect(factorial(1)).to.equal(1)
     expect(factorial(3)).to.equal(6)
     expect(factorial(4)).to.equal(24)
     expect(factorial(5)).to.equal(120)
